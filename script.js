ab0a193cc08fb7af23815bc0845fecc5aa8bb8d5
Niklas Kariniemi <niklaskariniemi@gmail.com>
2022-06-25 11:46:34 +0300

    Add functions for addition, subtractio, multiplication, and division

---
+++ script.js
@@ -0,0 +1,23 @@
+// function for addition
+function add( numOne, numTwo ){
+    num = numOne + numTwo;
+    return num;
+}
+
+// function for subtraction
+function sub( numOne, numTwo ){
+    num = numOne - numTwo;
+    return num;
+}
+
+// function for multiplication
+function multiply( numOne, numTwo ){
+    num = numOne * numTwo;
+    return num;
+}
+
+// function for division
+function divide( numOne, numTwo ){
+    num = numOne / numTwo;
+    return num;
+}
