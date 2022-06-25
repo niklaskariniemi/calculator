fd1c7c43be63e6dea4f519e2a35dcc540270357f
Niklas Kariniemi <niklaskariniemi@gmail.com>
2022-06-25 12:57:27 +0300

    Add event listener for every button on keypad

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// initialize variables
+const bottom = document.querySelector( '.bottom');
+const zero = document.querySelector( '#zero' );
+const one = document.querySelector( '#one' );
+const two = document.querySelector( '#two' );
+const three = document.querySelector( '#three' );
+const four = document.querySelector( '#four' );
+const five = document.querySelector( '#five' );
+const six = document.querySelector( '#six' );
+const seven = document.querySelector( '#seven' );
+const eight = document.querySelector( '#eight' );
+const nine = document.querySelector( '#nine' );
+const clear = document.querySelector( '#clear' );
+const equal = document.querySelector( '#equal' );
+const plus = document.querySelector( '#plus' );
+const minus = document.querySelector( '#minus' );
+const mult = document.querySelector( '#mult' );
+const division = document.querySelector( '#division' );
+
 // function for addition
 function add( numOne, numTwo ){
     num = numOne + numTwo;
@@ -47,3 +66,53 @@ function operate( operator, numOne, numTwo ){
 
     return num;
 }
+
+// event listener for every button on keypad
+zero.addEventListener('click', () => {
+    inputVal = bottom.innerText = '0';
+});
+one.addEventListener('click', () => {
+    inputVal = bottom.innerText = '1';
+});
+two.addEventListener('click', () => {
+    inputVal = bottom.innerText = '2';
+});
+three.addEventListener('click', () => {
+    inputVal = bottom.innerText = '3';
+});
+four.addEventListener('click', () => {
+    inputVal = bottom.innerText = '4';
+});
+five.addEventListener('click', () => {
+    inputVal = bottom.innerText = '5';
+});
+six.addEventListener('click', () => {
+    inputVal = bottom.innerText = '6';
+});
+seven.addEventListener('click', () => {
+    inputVal = bottom.innerText = '7';
+});
+eight.addEventListener('click', () => {
+    inputVal = bottom.innerText = '8';
+});
+nine.addEventListener('click', () => {
+    inputVal = bottom.innerText = '9';
+});
+clear.addEventListener('click', () => {
+    inputVal = bottom.innerText = 'C';
+});
+equal.addEventListener('click', () => {
+    inputVal = bottom.innerText = '=';
+});
+plus.addEventListener('click', () => {
+    inputVal = bottom.innerText = '+';
+});
+minus.addEventListener('click', () => {
+    inputVal = bottom.innerText = '-';
+});
+mult.addEventListener('click', () => {
+    inputVal = bottom.innerText = '*';
+});
+division.addEventListener('click', () => {
+    inputVal = bottom.innerText = '/';
+});
